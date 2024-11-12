540f64dcc0cbafc7e3f4093c70e84871d7782f0c
julibucci <buccijulieta0@gmail.com>
2024-11-12 12:21:36 -0300

    Create pag-premium-principal component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { PlanPremiumComponent } from './page/plan-premium/plan-premium.component
 import { ProfileComponent } from './profile/profile.component';
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 import { PaginaPrincipalComponent } from './page/pagina-principal/pagina-principal.component';
+import { PagPremiumPrincipalComponent } from './page/pag-premium-principal/pag-premium-principal.component';
 
 
 export const routes: Routes = [
@@ -30,6 +31,8 @@ export const routes: Routes = [
     path: 'plan-premium', 
     component: PlanPremiumComponent,
     children: [
+      { path: '', component: PagPremiumPrincipalComponent },
+      { path: 'home', component: PagPremiumPrincipalComponent },
       { path: 'profile', component: ProfileComponent },
       { path: 'recipes', component: RecipeListComponent },
     ]
