8003abd6183182c424af62c194341275fcf639ad
julibucci <buccijulieta0@gmail.com>
2024-11-13 19:52:09 -0300

    Arreglo de profile y auth component

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -4,6 +4,7 @@ import { Observable, throwError } from 'rxjs';
 import { map, catchError, switchMap } from 'rxjs/operators';
 import { LoginCredentials } from '../interfaces/loginCredentials';
 import { RegisterData } from '../interfaces/registerData';
+import { User } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -17,7 +18,9 @@ export class AuthService {
     return this.http.get<any[]>(`${this.apiUrl}?email=${credentials.email}&password=${credentials.password}`).pipe(
       map((users) => {
         if (users.length > 0) {
-          return { message: 'Login successful', user: users[0] };
+          const user = users[0];
+          localStorage.setItem('authUserId', user.id); // Guardamos el ID del usuario
+          return { message: 'Login successful', user: user };
         } else {
           throw new Error('Invalid email or password');
         }
@@ -25,18 +28,26 @@ export class AuthService {
       catchError((error) => throwError(() => new Error(error.message || 'Login failed')))
     );
   }
+  
 
   register(userData: RegisterData): Observable<any> {
+    const userWithDefaultPlan = { ...userData, userPlan: 'basic' };
+
     // Primero verificamos si el email ya está registrado
-    return this.http.get<any[]>(`${this.apiUrl}?email=${userData.email}`).pipe(
+    return this.http.get<User[]>(`${this.apiUrl}?email=${userWithDefaultPlan.email}`).pipe(
       switchMap((users) => {
         if (users.length > 0) {
           // Si el email ya existe, lanzamos un error
           return throwError(() => new Error('Email already registered'));
         } else {
-          // Si el email no existe, procedemos a crear el nuevo usuario
-          return this.http.post(this.apiUrl, userData).pipe(
-            map((user) => ({ message: 'Registration successful', user })),
+          // Si el email no existe, procedemos a crear el nuevo usuario con el plan 'basic'
+          return this.http.post<User>(this.apiUrl, userWithDefaultPlan).pipe(
+            map((user) => {
+              // Guardamos el ID del usuario y el plan en localStorage
+              localStorage.setItem('authUserId', user.id.toString());
+              localStorage.setItem('userPlan', user.userPlan);
+              return { message: 'Registration successful', user };
+            }),
             catchError((error) => throwError(() => new Error(error.message || 'Registration failed')))
           );
         }
@@ -44,12 +55,13 @@ export class AuthService {
       catchError((error) => throwError(() => new Error(error.message || 'Registration failed')))
     );
   }
+  
 
   logout(): void {
-    
+    localStorage.removeItem('authUserId');
     localStorage.removeItem('authToken');
     localStorage.removeItem('userPlan');
-    
   }
+  
 
 }
